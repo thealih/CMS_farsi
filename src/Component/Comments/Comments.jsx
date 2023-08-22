1a577df71a362424d5100c261805908b26e571e5
Ali.H <a.hashemi6336@gmail.com>
2023-08-22 23:20:30 +0330

    finish product section

--- src/Component/Comments/Comments.jsx
+++ src/Component/Comments/Comments.jsx
@@ -1,7 +1,28 @@
 import ErrorBox from "../ErrorBox/ErrorBox";
 
 const Comments = () => {
-  return <ErrorBox msg="هیچ کامنتی یافت نشد" />;
+  return (
+    <div className="cms-main">
+      <ErrorBox msg="هیچ کامنتی یافت نشد" />
+
+      <table className="cms-table">
+        <tr>
+          <th>اسم کاربر</th>
+          <th>محصول</th>
+          <th>کامنت</th>
+          <th>تاریخ</th>
+          <th>ساعت</th>
+        </tr>
+
+        <tr>
+          <td>علی</td>
+          <td>آیفون 13</td>
+          <td>1401-01-08</td>
+          <td>14:12</td>
+        </tr>
+      </table>
+    </div>
+  );
 };
 
 export default Comments;
